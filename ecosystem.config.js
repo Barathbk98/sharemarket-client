eea48848a127fa4c7ca3fe67c3c95eb84bb9d89b
barath kumar <barath.kumar@codingmart.com>
2020-02-12 01:08:30 +0530

    post deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -24,7 +24,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'https://github.com/Barathbk98/sharemarket-client.git',
       path : '/var/www/sharemarket-client',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'npm install && npm run build && pm2 reload ecosystem.config.js --env production'
     }
   }
 };
